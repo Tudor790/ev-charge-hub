1e63507a53818dbf044548af73174ed4769e1026
Todorache Tudor <tudor.todorache790@gmail.com>
2026-04-03 13:32:36 +0300

    Connect database with JPA - stations served from H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO charging_station (id, name_company, address, city, av_plugs, price_kwh, latitude, longitude)
+VALUES (1, 'Tesla A', 'Address A', 'City A', 4, 0.20, 40.7128, -74.0060);
+
+INSERT INTO charging_station (id, name_company, address, city, av_plugs, price_kwh, latitude, longitude)
+VALUES (2, 'Tesla B', 'Address B', 'City B', 6, 0.25, 34.0522, -118.2437);
+
+INSERT INTO charging_station (id, name_company, address, city, av_plugs, price_kwh, latitude, longitude)
+VALUES (3, 'Tesla C', 'Address C', 'City C', 8, 0.30, 33.7489, -84.3879);
